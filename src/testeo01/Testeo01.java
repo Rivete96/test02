fa1629b5add80a7d2fb2d7f4ee11889601e91252
rivete96 <rivete96@gmail.com>
2017-04-19 21:29:48 -0500

    primer avance

--- src/testeo01/Testeo01.java
+++ src/testeo01/Testeo01.java
@@ -15,7 +15,8 @@ public class Testeo01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //chachi hola
+       String chachi;
     }
     
 }
